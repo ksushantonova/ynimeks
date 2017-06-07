d63aa22cf73a5d95db01001bb05b7a55eae71d53
Ksu <msksushantonova@gmail.com>
2017-06-08 00:09:10 +0300

    добавлена передняя половина штанов с расчетами

--- index.js
+++ index.js
@@ -1,68 +1,59 @@
 
 /* Инпуты */
 
-var ot = document.getElementById("Ot");
-var ob = document.getElementById("Ob");
-var m3 = document.getElementById("m3");
-var h3 = document.getElementById("h3");
-var m5 = document.getElementById("m5");
-var h5 = document.getElementById("h5");
-var m7 = document.getElementById("m7");
-var h7 = document.getElementById("h7");
-var p =  document.getElementById("P");
-var tn = document.getElementById("TN");
-var vs = document.getElementById("TY");
-var tk = document.getElementById("TK");
-
-
-
-
-
-var N;
-var k;
-var ev;
-var otr;
-
-var f1;
-var f5;
-var f9;
-
-var ns;
+var ot = document.getElementById("Ot"),
+ ob = document.getElementById("Ob"),
+ m3 = document.getElementById("m3"),
+ h3 = document.getElementById("h3"),
+ m5 = document.getElementById("m5"),
+ h5 = document.getElementById("h5"),
+ m7 = document.getElementById("m7"),
+ h7 = document.getElementById("h7"),
+ p =  document.getElementById("P"),
+ tn = document.getElementById("TN"),
+ vs = document.getElementById("TY"),
+ tk = document.getElementById("TK"),
+ N,k,ev,otr,f1,f5,f9,ns;
 
 /* Функции */
 
 function getData(){
-ot = +ot.value;
-ob = +ob.value;
-m3 = +m3.value;
-h3 = +h3.value;
-m5 = +m5.value;
-h5 = +h5.value;
-m7 = +m7.value;
-h7 = +h7.value;
-tn = +tn.value;
-p = +p.value;
-vs = +vs.value;
-tk = +tk.value;
-
+ return {
+  ot: +ot.value,
+  ob: +ob.value,
+  m3: +m3.value,
+  h3: +h3.value,
+  m5: +m5.value,
+  h5: +h5.value,
+  m7: +m7.value,
+  h7: +h7.value,
+  tn: +tn.value,
+  p:  +p.value,
+  vs: +vs.value,
+  tk: +tk.value
+ }
 };
+
+var data = getData();
 
 var Np = function(){
 
-Np = "aaa";
+return "aaa"
 
 };
 
+var Nsr = (N + Np) / 2;
+
 function basic(){
 
-N = ob / 20;
+N = data.ob / 20;
 k = N / 2;
-ev = (m3 + m5 + m7) * 2;
-otr = ob - ev;
+ev = (data.m3 + data.m5 + data.m7) * 2;
+otr = data.ob - ev;
 
-f1 = (m3 - N) / 2;
-f5 = (m5 - N) / 2;
-f9 = (m7 - N) / 2;
+f1 = (data.m3 - N) / 2;
+f5 = (data.m5 - N) / 2;
+f9 = (data.m7 - N) / 2;
 
 ns = (N + Np) / 2;
 
@@ -75,43 +66,43 @@ console.log("N = " + N +", k = "+ k + ", ev = "+ ev + ", otr = " + otr + ", f1 =
 
 function verifyAll(){
 	var result;
-   if (otr > ot){
-   	result = otr - ot;
+   if (otr > data.ot){
+   	result = otr - data.ot;
    	if (result > 4){
    		alert("Подкорректируйте мерки!");
    	} else {
-   		m3 = m3 + (result/6);
-		m5 = m5 + (result/6);
-		m7 = m7 + (result/6);
+   		data.m3 = data.m3 + (result/6);
+		data.m5 = data.m5 + (result/6);
+		data.m7 = data.m7 + (result/6);
    	}
    } else {
-   		result = ot - otr;
+   		result = data.ot - otr;
    	 if(result > 4){
    	 	alert("Подкорректируйте мерки!");
    	 } else {
-   	 	m3 = m3 - (result/6);
-		m5 = m5 - (result/6);
-		m7 = m7 - (result/6);
+   	 	data.m3 = data.m3 - (result/6);
+		data.m5 = data.m5 - (result/6);
+		data.m7 = data.m7 - (result/6);
    	 }
    }
 
 
-	console.log("new m3 = " + m3 + ", new m5 = " + m5 + ", new m7 = " + m7)
+	console.log("new m3 = " + data.m3 + ", new m5 = " + data.m5 + ", new m7 = " + data.m7)
 };
 
 
 
 function square(){
 	var result;
-	if (m7 <= N){
+	if (data.m7 <= N){
 		result = k/2;
-	} else if ((m7 > N) && (m7 < (1.5 * N))) {
+	} else if ((data.m7 > N) && (data.m7 < (1.5 * N))) {
 		result =  k*0.75;
-	} else if (m7 == (1.5 * N)){
+	} else if (data.m7 == (1.5 * N)){
 		result = k;
-	} else if (m7 > (1.5*N)){
+	} else if (data.m7 > (1.5*N)){
 		result = 1.5*k;
-	} else if (m7 > (2.5*N)){
+	} else if (data.m7 > (2.5*N)){
 		result = N;
 	};
 	console.log("square = " + result)
@@ -180,7 +171,7 @@ var v2z;
 
 
 function vytachkiCount(){
-	if (m3 <= k ){
+	if (data.m3 <= k ){
 		if (figure == "bigj"){
 			v1p = 3 * N;
 			v2p = 0;
@@ -189,7 +180,7 @@ function vytachkiCount(){
 			v2p = 0;
 		};
 
-	} else if (m3 > k ){
+	} else if (data.m3 > k ){
 		if (figure == "bigass"){
 			v1p = 1.5 * N;
 			v2p = 3 * N;
@@ -200,10 +191,10 @@ function vytachkiCount(){
 		
 	};
 
-	if (m7 <= N){
+	if (data.m7 <= N){
 		v1z = 0;
 		v2z = 2.5 * N;
-	} else if ( m7 > N){
+	} else if ( data.m7 > N){
 		v1z = 1.5 * N;
 		v2z = 3 * N;
 	}
@@ -221,9 +212,9 @@ function razmerVytach(){
 	// расчет m5, m3
 var oss1 = 0;
 var oss2 = 0;
-	if (m5 > (1.25 * N)){
+	if (data.m5 > (1.25 * N)){
 		m5v = 1.25 * N;
-		oss1 = m5 - m5v;
+		oss1 = data.m5 - m5v;
 		if(oss1 < k){
 			m7v1 += +oss1;
 		} else {
@@ -231,15 +222,15 @@ var oss2 = 0;
 			oss2 = oss1 - k;
 			}
 	} else {
-		m5v = m5;
+		m5v = data.m5;
 		m7v1 += 0;
 	}
 
- if (m3 > k){
- 	m3v1 = (m3 + oss2)/2;
- 	m3v2 = (m3 + oss2)/2;
+ if (data.m3 > k){
+ 	m3v1 = (data.m3 + oss2)/2;
+ 	m3v2 = (data.m3 + oss2)/2;
  } else {
- 	m3v1 = m3 + oss2;
+ 	m3v1 = data.m3 + oss2;
  	m3v2 = 0;
  }
 
@@ -248,9 +239,9 @@ var oss3 = 0;
 
 // расчет m7
 
-	if (m7 > N){
+	if (data.m7 > N){
 		m7v2 = N;
-		oss3 = m7 - m7v2;
+		oss3 = data.m7 - m7v2;
 		if (oss3 < k){
 			m7v1 = +m7v1 + +oss3;
 			m9v = 0;
@@ -259,7 +250,7 @@ var oss3 = 0;
 			m9v = oss3 - k;
 		}
 	} else {
-		m7v2 = m7;
+		m7v2 = data.m7;
 		m7v1 = m7v1;
 		m9v = 0;
 	}
@@ -267,6 +258,35 @@ var oss3 = 0;
 
 	console.log("m5v = " + m5v + ", m3v1 = " + m3v1 + ", m3v2 = " + m3v2 + ", m7v1 = " + m7v1 + ", m7v2 = " + m7v2 + ", m9v = " + m9v);
 }
+
+var vep,vsp,vdp1,nop,vnp,kp,tnp,tkp,evp,kpn,ppp,vpp,res;
+
+function getWidthSh(){
+  	if (figure == "bigj" || figure == "bigass"){
+  		res = 2.25 * Nsr;
+  	} if (figure == "normal") {
+  		res = 2.25 * N;
+  	}
+  	return res;
+  };
+
+function getShtanyP(){
+  vep = (5 * N) - (k/2);
+  vsp = data.vs;
+  vdp1 = ((vep) + N) /2;
+  nop = N;
+  vnp = 1.5 * N;
+  kp = k;
+  tnp = data.tn;
+  tkp = data.tk;
+  evp = 3 * N;
+  kpn = k/4;
+  tkn = data.tn - data.tk; 
+   ppp = vpp = res;
+   nppp = nvpp = res;
+}
+
+
 
 /* Скрипт */
 
@@ -316,8 +336,29 @@ document.getElementById("hp1").innerHTML = Math.floor((getBal() * N)* 100)/100;
 
 }
 
+function setValueShtanyP(){
+	document.getElementById("vep").innerHTML = Math.floor((vep)*100)/100;
+	document.getElementById("vsp").innerHTML = Math.floor((vsp)*100)/100;
+	document.getElementById("vdp1").innerHTML = "1/2(" + Math.floor((vdp1)*100)/100 + ")";
+	document.getElementById("nop").innerHTML = Math.floor((nop)*100)/100;
+	document.getElementById("vnp").innerHTML = Math.floor((vnp)*100)/100;
+	document.getElementById("kp").innerHTML = Math.floor((kp)*100)/100;
+	document.getElementById("tnp").innerHTML = Math.floor((tnp)*100)/100;
+	document.getElementById("tkp").innerHTML = Math.floor((tkp)*100)/100;
+	document.getElementById("evp").innerHTML = Math.floor((evp)*100)/100;
+	document.getElementById("kpn").innerHTML = Math.floor((kpn)*100)/100;
+	document.getElementById("ppp").innerHTML = Math.floor((ppp)*100)/100;
+	document.getElementById("vpp").innerHTML = Math.floor((vpp)*100)/100;
+	document.getElementById("nppp").innerHTML = Math.floor((ppp)*100)/100;
+	document.getElementById("nvpp").innerHTML = Math.floor((vpp)*100)/100;
+	document.getElementById("tkn").innerHTML = Math.floor((tkn)*100)/100;
+
+}
+
+
+						      // финальная функция
+
 function main(){
-getData();
 getFigure();
 	console.log(getFigure());
 	getBal();
@@ -332,6 +373,10 @@ getFigure();
     vytachkiCount();
      razmerVytach();
      setValueVytochki();
+      getWidthSh();
+     getShtanyP();
+     setValueShtanyP();
+
 };
 
-//сделала полность вытачки. пора делать остальное
+
